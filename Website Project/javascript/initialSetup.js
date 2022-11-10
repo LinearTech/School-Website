76aa423bd236070465d17e7952aa80dfdb828cf3
LTCodes5030 <115086945+LTCodes5030@users.noreply.github.com>
2022-11-10 12:32:14 -0500

    Add files via upload

--- Website Project/javascript/initialSetup.js
+++ Website Project/javascript/initialSetup.js
@@ -1,4 +1,5 @@
-document.onload = setResolution();
+document.onload = load();
+
 function setResolution(){
 	element1 = document.getElementById("target");
 	if ( ( screen.width == 1366 ) && ( screen.height == 768 ) ) {
@@ -8,4 +9,16 @@ function setResolution(){
 	{
 		element1.insertAdjacentHTML("afterbegin", "<link rel='stylesheet' href='../css/mainCss.css'><link rel='icon' href='../graphics/icons/cte_site_icon.png'><style>html{background-image: url('../graphics/bg/mainBg.jpg');background-size: 2500px, 1600px;}</style>");
 	}
+}
+
+function dateSet()
+{
+		var date = new Date();
+		var currentDate = (date.getMonth() + 1) + "/" + date.getDate() + "/" + date.getFullYear();
+		document.getElementById("p1").innerHTML = currentDate;
+}
+function load()
+{
+	setResolution(); 
+	dateSet();
 }
